072807362ecf4c84512cd2bd265b236f011ad768
Erik <erikwestberg7@gmail.com>
2022-04-08 18:56:36 +0200

    Solution restructure

---
+++ SharpMeasures/SharpMeasures/UnitOfTime.cs
@@ -0,0 +1,19 @@
+﻿namespace ErikWe.SharpMeasures;
+
+using ErikWe.SharpMeasures.Attributes;
+using ErikWe.SharpMeasures.Attributes.Utility;
+
+using System;
+
+//[Unit(typeof(Length))]
+//[FixedUnitInstance("Metre", UnitPluralCodes.AppendS, 1)]
+//public readonly partial record struct UnitOfLength { }
+
+//[Unit(typeof(Time))]
+//[FixedUnitInstance("Second", UnitPluralCodes.AppendS, 1)]
+//public readonly partial record struct UnitOfTime { }
+
+//[Unit(typeof(Speed))]
+//[DerivedUnit(UnitDerivations.Division, typeof(UnitOfLength), typeof(UnitOfTime))]
+//[DerivedUnitInstance("MetrePerSecond", UnitPluralCodes.InsertSBeforePer, new Type[] { typeof(UnitOfLength), typeof(UnitOfTime) }, new string[] { "Metre", "Second" })]
+//public readonly partial record struct UnitOfSpeed { }
