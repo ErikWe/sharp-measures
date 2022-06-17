28af51923bae8a9f69e755043c4e1912d426162a
Erik <erikwestberg7@gmail.com>
2022-06-17 18:07:42 +0200

    Include project dependency dlls in analyzers/dotnet/cs

--- SharpMeasures/SharpMeasures/UnitOfLength.cs
+++ SharpMeasures/SharpMeasures/UnitOfLength.cs
@@ -1,10 +1,6 @@
 ﻿namespace SharpMeasures;
 
-using SharpMeasures.Generators.Scalars;
 using SharpMeasures.Generators.Units;
-
-[SharpMeasuresScalar(typeof(UnitOfLength))]
-public partial class Length { }
 
 [SharpMeasuresUnit(typeof(Length), BiasTerm = false)]
 public readonly partial record struct UnitOfLength { }
