a9a060c66e39d77d7c0b206f967ed553eddc631b
Erik <erikwestberg7@gmail.com>
2022-07-29 18:40:56 +0200

    Passing TypeNotVector tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/TypeNotVector.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/TypeNotVector.cs
@@ -13,32 +13,111 @@ using Xunit;
 [UsesVerify]
 public class TypeNotVector
 {
+    [Fact]
+    public Task VerifyTypeNotScalarDiagnosticsMessage_Null()
+    {
+        var source = ScalarVectorText("null");
+
+        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
+    }
+
+    [Fact]
+    public Task VerifyTypeNotScalarDiagnosticsMessage_Int()
+    {
+        var source = ScalarVectorText("typeof(int)");
+
+        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
+    }
+
+    [Theory]
+    [MemberData(nameof(NonVectorTypes))]
+    public void ScalarVector_ExactList(string value)
+    {
+        string source = ScalarVectorText(value);
+
+        AssertExactlyTypeNotVectorDiagnostics(source);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonVectorTypes))]
+    public void SpecializedVectorOriginalVector_ExactList(string value)
+    {
+        string source = SpecializedVectorOriginalVectorText(value);
+
+        AssertExactlyTypeNotVectorDiagnostics(source);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonVectorTypes))]
+    public void VectorDifference_ExactList(string value)
+    {
+        string source = VectorDifferenceText(value);
+
+        AssertExactlyTypeNotVectorDiagnostics(source);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonVectorTypes))]
+    public void SpecializedVectorDifference_ExactList(string value)
+    {
+        string source = SpecializedVectorDifferenceText(value);
+
+        AssertExactlyTypeNotVectorDiagnostics(source);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonVectorTypes))]
+    public void ConvertibleQuantity_ExactList(string value)
+    {
+        if (value is "null")
+        {
+            value = "(System.Type)null";
+        }
+
+        var source = ConvertibleQuantityText(value);
+
+        AssertExactlyTypeNotVectorDiagnostics(source);
+    }
+
+    private static IEnumerable<object[]> NonVectorTypes() => new object[][]
+    {
+        new[] { "null" },
+        new[] { "typeof(int)" },
+        new[] { "typeof(UnitOfLength)" },
+        new[] { "typeof(Length)" }
+    };
+
+    private static GeneratorVerifier AssertExactlyTypeNotVectorDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(TypeNotVectorDiagnostics);
+    private static IReadOnlyCollection<string> TypeNotVectorDiagnostics { get; } = new string[] { DiagnosticIDs.TypeNotVector };
+
     private static string ScalarVectorText(string value) => $$"""
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
 
         [SharpMeasuresScalar(typeof(UnitOfLength), Vector = {{value}})]
+        public partial class Distance { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
         public partial class Length { }
 
         [SharpMeasuresUnit(typeof(Length))]
         public partial class UnitOfLength { }
         """;
 
-    [Fact]
-    public Task ScalarVector_Null_ExactListAndVerify()
-    {
-        string source = ScalarVectorText("null");
+    private static string SpecializedVectorOriginalVectorText(string value) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
 
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
+        [SpecializedSharpMeasuresVector({{value}})]
+        public partial class Displacement3 { }
 
-    [Fact]
-    public Task ScalarVector_Int_ExactListAndVerify()
-    {
-        string source = ScalarVectorText("typeof(int)");
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
 
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
 
     private static string VectorDifferenceText(string value) => $$"""
         using SharpMeasures.Generators.Scalars;
@@ -55,34 +134,16 @@ public class TypeNotVector
         public partial class UnitOfLength { }
         """;
 
-    [Fact]
-    public Task VectorDifference_Null_ExactListAndVerify()
-    {
-        string source = VectorDifferenceText("null");
-
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
-
-    [Fact]
-    public Task VectorDifference_Int_ExactListAndVerify()
-    {
-        string source = VectorDifferenceText("typeof(int)");
-
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
-
-    private static string DimensionalEquivalenceText(string value) => $$"""
-        using SharpMeasures.Generators.Quantities;
+    private static string SpecializedVectorDifferenceText(string value) => $$"""
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
-        [DimensionalEquivalence({{value}})]
-        [SharpMeasuresVector(typeof(UnitOfLength))]
-        public partial class Position3 { }
+        [SpecializedSharpMeasuresVector(typeof(Position3), Difference = {{value}})]
+        public partial class Displacement3 { }
 
         [SharpMeasuresVector(typeof(UnitOfLength))]
-        public partial class Displacement3 { }
+        public partial class Position3 { }
 
         [SharpMeasuresScalar(typeof(UnitOfLength))]
         public partial class Length { }
@@ -91,24 +152,20 @@ public class TypeNotVector
         public partial class UnitOfLength { }
         """;
 
-    [Fact]
-    public Task DimensionalEquivalence_Null_ExactListAndVerify()
-    {
-        string source = DimensionalEquivalenceText("typeof(Displacement3), null");
+    private static string ConvertibleQuantityText(string value) => $$"""
+        using SharpMeasures.Generators.Quantities;
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
 
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
+        [ConvertibleQuantity({{value}})]
+        [SharpMeasuresVector(typeof(UnitOfLength))]
+        public partial class Position3 { }
 
-    [Fact]
-    public Task DimensionalEquivalence_Int_ExactListAndVerify()
-    {
-        string source = DimensionalEquivalenceText("typeof(int)");
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
 
-        return AssertExactlyTypeNotVectorDiagnostics(source).VerifyDiagnostics();
-    }
-
-    private static GeneratorVerifier AssertExactlyTypeNotVectorDiagnostics(string source)
-        => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(TypeNotVectorDiagnostics);
-
-    private static IReadOnlyCollection<string> TypeNotVectorDiagnostics { get; } = new string[] { DiagnosticIDs.TypeNotVector };
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
 }
