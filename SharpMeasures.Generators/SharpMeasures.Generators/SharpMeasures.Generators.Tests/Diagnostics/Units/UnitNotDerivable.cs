06127f89b59e60d623c21ca432355afd0fdd4f5b
Erik <erikwestberg7@gmail.com>
2022-07-31 05:34:22 +0200

    Passing UnitNotDerivable tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/UnitNotDerivable.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/UnitNotDerivable.cs
@@ -14,7 +14,7 @@ using Xunit;
 public class UnitNotDerivable
 {
     [Fact]
-    public Task ExactListAndVerify()
+    public Task WithoutID_ExactListAndVerify()
     {
         string source = """
             using SharpMeasures.Generators.Scalars;
@@ -23,7 +23,7 @@ public class UnitNotDerivable
             [SharpMeasuresScalar(typeof(UnitOfLength))]
             public partial class Length { }
 
-            [DerivedUnit("Metre", "Metres", "1", "MetrePerSecond", "Second")]
+            [DerivedUnit("Metre", "Metres", new[] { "MetrePerSecond", "Second" })]
             [SharpMeasuresUnit(typeof(Length))]
             public partial class UnitOfLength { }
             """;
@@ -31,8 +31,24 @@ public class UnitNotDerivable
         return AssertExactlyUnitNotDerivableDiagnostics(source).VerifyDiagnostics();
     }
 
-    private static GeneratorVerifier AssertExactlyUnitNotDerivableDiagnostics(string source)
-        => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(UnitNotDerivableDiagnostics);
+    [Fact]
+    public void WithID_ExactList()
+    {
+        string source = """
+            using SharpMeasures.Generators.Scalars;
+            using SharpMeasures.Generators.Units;
 
+            [SharpMeasuresScalar(typeof(UnitOfLength))]
+            public partial class Length { }
+
+            [DerivedUnit("Metre", "Metres", "1", new[] { "MetrePerSecond", "Second" })]
+            [SharpMeasuresUnit(typeof(Length))]
+            public partial class UnitOfLength { }
+            """;
+
+        AssertExactlyUnitNotDerivableDiagnostics(source);
+    }
+
+    private static GeneratorVerifier AssertExactlyUnitNotDerivableDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(UnitNotDerivableDiagnostics);
     private static IReadOnlyCollection<string> UnitNotDerivableDiagnostics { get; } = new string[] { DiagnosticIDs.UnitNotDerivable };
 }
