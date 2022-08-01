9e173218a918c68122f92255271e57087d09ea08
Erik <erikwestberg7@gmail.com>
2022-08-01 16:22:24 +0200

    Added passing TypeNotBiased tests

---
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/TypeNotBiasedScalar.cs
@@ -0,0 +1,39 @@
+﻿namespace SharpMeasures.Generators.Tests.Diagnostics;
+
+using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Verify;
+
+using System.Collections.Generic;
+
+using VerifyXunit;
+
+using Xunit;
+
+[UsesVerify]
+public class TypeNotBiasedScalar
+{
+    [Fact]
+    public void ConvertibleScalar_ExactList()
+    {
+        string source = """
+            using SharpMeasures.Generators.Quantities;
+            using SharpMeasures.Generators.Scalars;
+            using SharpMeasures.Generators.Units;
+
+            [ConvertibleQuantity(typeof(TemperatureDifference))]
+            [SharpMeasuresScalar(typeof(UnitOfTemperature), UseUnitBias = true)]
+            public partial class Temperature { }
+            
+            [SharpMeasuresScalar(typeof(UnitOfTemperature))]
+            public partial class TemperatureDifference { }
+
+            [SharpMeasuresUnit(typeof(TemperatureDifference), BiasTerm = true)]
+            public partial class UnitOfTemperature { }
+            """;
+
+        AssertExactlyTypeNotBiasedScalarDiagnostics(source);
+    }
+
+    private static GeneratorVerifier AssertExactlyTypeNotBiasedScalarDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(TypeNotBiasedDiagnostics);
+    private static IReadOnlyCollection<string> TypeNotBiasedDiagnostics { get; } = new string[] { DiagnosticIDs.TypeNotBiasedScalar };
+}
