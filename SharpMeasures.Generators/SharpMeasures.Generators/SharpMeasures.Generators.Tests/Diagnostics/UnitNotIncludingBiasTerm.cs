3b2e294c38e51667cd609be6dc4dc3e88f00d126
Erik <erikwestberg7@gmail.com>
2022-08-02 16:22:47 +0200

    Added passing UnitNotIncludingBiasTerm tests

---
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/UnitNotIncludingBiasTerm.cs
@@ -0,0 +1,39 @@
+﻿namespace SharpMeasures.Generators.Tests.Diagnostics;
+
+using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Verify;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using VerifyXunit;
+
+using Xunit;
+
+[UsesVerify]
+public class UnitNotIncludingBiasTerm
+{
+    [Fact]
+    public Task BiasedScalar_ExactListAndVerify()
+    {
+        string source = $$"""
+            using SharpMeasures.Generators.Quantities;
+            using SharpMeasures.Generators.Scalars;
+            using SharpMeasures.Generators.Units;
+
+            [SharpMeasuresScalar(typeof(UnitOfLength), UseUnitBias = true)]
+            public partial class Temperature { }
+            
+            [SharpMeasuresScalar(typeof(UnitOfLength))]
+            public partial class Length { }
+
+            [SharpMeasuresUnit(typeof(Length))]
+            public partial class UnitOfLength { }
+            """;
+
+        return AssertExactlyUnitNotIncludingBiasTermDiagnostics(source).VerifyDiagnostics();
+    }
+
+    private static GeneratorVerifier AssertExactlyUnitNotIncludingBiasTermDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(UnitNotIncludingBiasTermDiagnostics);
+    private static IReadOnlyCollection<string> UnitNotIncludingBiasTermDiagnostics { get; } = new string[] { DiagnosticIDs.UnitNotIncludingBiasTerm };
+}
