ed8df75c177ebb325ee161e937c45b42e683722a
Erik <erikwestberg7@gmail.com>
2022-08-03 18:50:46 +0200

    Passing DuplicateConstantName tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantMultiplesName.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantMultiplesName.cs
@@ -14,137 +14,155 @@ using Xunit;
 public class DuplicateConstantMultiplesName
 {
     [Fact]
-    public Task Scalar_Explicit_ExactListAndVerify()
+    public Task VerifyDuplicateConstantMultiplesNameDiagnosticsMessage()
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            [ScalarConstant("Planck", "Metre", 1.616255E-35, Multiples = "Plancks")]
-            [ScalarConstant("Planck2", "Metre", 1.616255E-35, Multiples = "Plancks")]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
+        string source = ScalarText(", Multiples = \"MultiplesOfPlancks\"", ", Multiples = \"MultiplesOfPlancks\"");
 
         return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
     }
 
-    [Fact]
-    public Task Scalar_ImplicitFirst_ExactListAndVerify()
+    [Theory]
+    [MemberData(nameof(DuplicateConstantMultiplesNames))]
+    public void Scalar_ExactList(string firstArgument, string secondArgument)
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
+        string source = ScalarText(firstArgument, secondArgument);
 
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-            [ScalarConstant("Planck2", "Metre", 1.616255E-35, Multiples = "MultiplesOfPlanck")]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
     }
 
-    [Fact]
-    public Task Scalar_ExplicitFirst_ExactListAndVerify()
+    [Theory]
+    [MemberData(nameof(DuplicateConstantMultiplesNames))]
+    public void SpecializedScalar_ExactList(string firstArgument, string secondArgument)
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
+        string source = SpecializedScalarText(firstArgument, secondArgument);
 
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            [ScalarConstant("Planck2", "Metre", 1.616255E-35, Multiples = "MultiplesOfPlanck")]
-            [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
     }
 
-    [Fact]
-    public Task Vector_Explicit_ExactListAndVerify()
+    [Theory]
+    [MemberData(nameof(DuplicateConstantMultiplesNames))]
+    public void Vector_ExactList(string firstArgument, string secondArgument)
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-            using SharpMeasures.Generators.Vectors;
+        string source = VectorText(firstArgument, secondArgument);
 
-            [VectorConstant("MetreOnes", "Metre", 1, 1, 1, Multiples = "InMetreOnes")]
-            [VectorConstant("MetreOnes2", "Metre", 1, 1, 1, Multiples = "InMetreOnes")]
-            [SharpMeasuresVector(typeof(UnitOfLength))]
-            public partial class Position3 { }
-
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
     }
 
-    [Fact]
-    public Task Vector_ImplicitFirst_ExactListAndVerify()
+    [Theory]
+    [MemberData(nameof(DuplicateConstantMultiplesNames))]
+    public void SpecializedVector_ExactList(string firstArgument, string secondArgument)
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-            using SharpMeasures.Generators.Vectors;
+        string source = SpecializedVectorText(firstArgument, secondArgument);
 
-            [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-            [VectorConstant("MetreOnes2", "Metre", 1, 1, 1, Multiples = "MultiplesOfMetreOnes")]
-            [SharpMeasuresVector(typeof(UnitOfLength))]
-            public partial class Position3 { }
-
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
     }
 
-    [Fact]
-    public Task Vector_ExplicitFirst_ExactListAndVerify()
+    [Theory]
+    [MemberData(nameof(DuplicateConstantMultiplesNames))]
+    public void VectorGroupMember_ExactList(string firstArgument, string secondArgument)
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-            using SharpMeasures.Generators.Vectors;
+        string source = VectorGroupMemberText(firstArgument, secondArgument);
 
-            [VectorConstant("MetreOnes2", "Metre", 1, 1, 1, Multiples = "MultiplesOfMetreOnes")]
-            [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-            [SharpMeasuresVector(typeof(UnitOfLength))]
-            public partial class Position3 { }
-
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
     }
+
+    private static IEnumerable<object[]> DuplicateConstantMultiplesNames() => new object[][]
+    {
+        new[] { ", Multiples = \"MultiplesOfPlancks\"", ", Multiples = \"MultiplesOfPlancks\"" },
+        new[] { string.Empty, ", Multiples = \"MultiplesOfPlancks\"" }
+    };
 
     private static GeneratorVerifier AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantMultiplesNameDiagnostics).AssertAllDiagnosticsValidLocation();
     private static IReadOnlyCollection<string> DuplicateConstantMultiplesNameDiagnostics { get; } = new string[] { DiagnosticIDs.DuplicateConstantMultiplesName };
+
+    private static string ScalarText(string firstArgument, string secondArgument) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
+        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string SpecializedScalarText(string firstArgument, string secondArgument) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
+        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [SpecializedSharpMeasuresScalar(typeof(Length))]
+        public partial class Distance { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string VectorText(string firstArgument, string secondArgument) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
+        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [SharpMeasuresVector(typeof(UnitOfLength))]
+        public partial class Position3 { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string SpecializedVectorText(string firstArgument, string secondArgument) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
+        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [SpecializedSharpMeasuresVector(typeof(Position3))]
+        public partial class Displacement3 { }
+
+        [SharpMeasuresVector(typeof(UnitOfLength))]
+        public partial class Position3 { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string VectorGroupMemberText(string firstArgument, string secondArgument) => $$"""
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
+        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [SharpMeasuresVectorGroupMember(typeof(Position))]
+        public partial class Position3 { }
+
+        [SharpMeasuresVectorGroup(typeof(UnitOfLength))]
+        public static partial class Position { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
 }
--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantName.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantName.cs
@@ -16,47 +16,121 @@ public class DuplicateConstantName
     [Fact]
     public Task Scalar_ExactListAndVerify()
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-            [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(ScalarText).VerifyDiagnostics();
     }
 
     [Fact]
-    public Task Vector_ExactListAndVerify()
+    public Task SpecializedScalar_ExactListAndVerify()
     {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
-            using SharpMeasures.Generators.Vectors;
+        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(SpecializedScalarText).VerifyDiagnostics();
+    }
 
-            [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-            [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-            [SharpMeasuresVector(typeof(UnitOfLength))]
-            public partial class Position3 { }
+    [Fact]
+    public void Vector_ExactList()
+    {
+        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(VectorText);
+    }
 
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-            
-            [FixedUnit("Metre", "Metres")]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
+    [Fact]
+    public void SpecializedVector_ExactList()
+    {
+        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(SpecializedVectorText);
+    }
 
-        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
+    [Fact]
+    public void VectorGroupMember_ExactList()
+    {
+        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(VectorGroupMemberText);
     }
 
     private static GeneratorVerifier AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantNameDiagnostics).AssertAllDiagnosticsValidLocation();
     private static IReadOnlyCollection<string> DuplicateConstantNameDiagnostics { get; } = new string[] { DiagnosticIDs.DuplicateConstantName };
+
+    private static string ScalarText => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string SpecializedScalarText => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [SpecializedSharpMeasuresScalar(typeof(Length))]
+        public partial class Distance { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string VectorText => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [SharpMeasuresVector(typeof(UnitOfLength))]
+        public partial class Position3 { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string SpecializedVectorText => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [SpecializedSharpMeasuresVector(typeof(Position3))]
+        public partial class Displacement3 { }
+
+        [SharpMeasuresVector(typeof(UnitOfLength))]
+        public partial class Position3 { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static string VectorGroupMemberText => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+        using SharpMeasures.Generators.Vectors;
+
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [SharpMeasuresVectorGroupMember(typeof(Position))]
+        public partial class Position3 { }
+
+        [SharpMeasuresVectorGroup(typeof(UnitOfLength))]
+        public static partial class Position { }
+            
+        [FixedUnit("Metre", "Metres")]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
 }
