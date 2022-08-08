35b1b705e734c9f8d10059415949393cc26d48b8
Erik <erikwestberg7@gmail.com>
2022-08-08 20:08:19 +0200

    Passing BiasedUnitDefinedButUnitNotBiased tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/BiasedUnitDefinedButUnitNotBiased.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/BiasedUnitDefinedButUnitNotBiased.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Units;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,24 +15,28 @@ using Xunit;
 public class BiasedUnitDefinedButUnitNotBiased
 {
     [Fact]
-    public Task ExactListAndVerify()
-    {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
+    public Task VerifyBiasedUnitDefinedButUnitNotBiasedDiagnosticsMessage() => Assert().VerifyDiagnostics();
 
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-
-            [FixedUnit("Metre", "Metres")]
-            [BiasedUnit("Metre2", "Metres2", "Metre", 1)]
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-            """;
-
-        return AssertExactlyBiasedUnitDefinedButUnitNotBiasedDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
-
-    private static GeneratorVerifier AssertExactlyBiasedUnitDefinedButUnitNotBiasedDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(BiasedUnitDefinedButUnitNotBiasedDiagnostics).AssertAllDiagnosticsValidLocation();
+    private static GeneratorVerifier AssertExactlyBiasedUnitDefinedButUnitNotBiasedDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(BiasedUnitDefinedButUnitNotBiasedDiagnostics);
     private static IReadOnlyCollection<string> BiasedUnitDefinedButUnitNotBiasedDiagnostics { get; } = new string[] { DiagnosticIDs.BiasedUnitDefinedButUnitNotBiased };
+
+    private static string Text => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+
+        [FixedUnit("Metre", "Metres")]
+        [BiasedUnit("Metre2", "Metres2", "Metre", 1)]
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+        """;
+
+    private static GeneratorVerifier Assert()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(Text, target: "BiasedUnit", postfix: "(\"Metre2\", \"Metres2\", \"Metre\", 1)]");
+
+        return AssertExactlyBiasedUnitDefinedButUnitNotBiasedDiagnostics(Text).AssertDiagnosticsLocation(expectedLocation, Text);
+    }
 }
