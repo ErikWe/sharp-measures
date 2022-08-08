1d28659a4a7b8b3facb01eafed47d0b6796b793b
Erik <erikwestberg7@gmail.com>
2022-08-08 23:43:27 +0200

    Passing DuplicateUnitDerivationID tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/DuplicateUnitDerivationID.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/DuplicateUnitDerivationID.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Units;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,36 +15,40 @@ using Xunit;
 public class DuplicateUnitDerivationID
 {
     [Fact]
-    public Task ExactListAndVerify()
-    {
-        string source = """
-            using SharpMeasures.Generators.Scalars;
-            using SharpMeasures.Generators.Units;
+    public Task VerifyDuplicateUnitDerivationIDDiagnosticsMessage() => Assert().VerifyDiagnostics();
 
-            [SharpMeasuresScalar(typeof(UnitOfLength))]
-            public partial class Length { }
-
-            [SharpMeasuresScalar(typeof(UnitOfTime))]
-            public partial class Time { }
-
-            [SharpMeasuresScalar(typeof(UnitOfSpeed))]
-            public partial class Speed { }
-
-            [SharpMeasuresUnit(typeof(Length))]
-            public partial class UnitOfLength { }
-
-            [SharpMeasuresUnit(typeof(Time))]
-            public partial class UnitOfTime { }
-
-            [DerivableUnit("id", "{0} / {1}", typeof(UnitOfLength), typeof(UnitOfTime))]
-            [DerivableUnit("id", "{0} / {1}", typeof(UnitOfLength), typeof(UnitOftime))]
-            [SharpMeasuresUnit(typeof(Speed))]
-            public partial class UnitOfSpeed { }
-            """;
-
-        return AssertExactlyDuplicateUnitDerivationIDDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
-
-    private static GeneratorVerifier AssertExactlyDuplicateUnitDerivationIDDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateUnitderivationIDDiagnostics).AssertAllDiagnosticsValidLocation();
+    private static GeneratorVerifier AssertExactlyDuplicateUnitDerivationIDDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateUnitderivationIDDiagnostics);
     private static IReadOnlyCollection<string> DuplicateUnitderivationIDDiagnostics { get; } = new string[] { DiagnosticIDs.DuplicateUnitDerivationID };
+
+    private static string Text => """
+        using SharpMeasures.Generators.Scalars;
+        using SharpMeasures.Generators.Units;
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
+
+        [SharpMeasuresScalar(typeof(UnitOfTime))]
+        public partial class Time { }
+
+        [SharpMeasuresScalar(typeof(UnitOfSpeed))]
+        public partial class Speed { }
+
+        [SharpMeasuresUnit(typeof(Length))]
+        public partial class UnitOfLength { }
+
+        [SharpMeasuresUnit(typeof(Time))]
+        public partial class UnitOfTime { }
+
+        [DerivableUnit("id", "{0} / {1}", typeof(UnitOfLength), typeof(UnitOfTime))]
+        [DerivableUnit("id", "{0} / {1}", typeof(UnitOfLength), typeof(UnitOfTime))] // <-
+        [SharpMeasuresUnit(typeof(Speed))]
+        public partial class UnitOfSpeed { }
+        """;
+
+    private static GeneratorVerifier Assert()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(Text, target: "\"id\"", postfix: ", \"{0} / {1}\", typeof(UnitOfLength), typeof(UnitOfTime))] // <-");
+
+        return AssertExactlyDuplicateUnitDerivationIDDiagnostics(Text).AssertDiagnosticsLocation(expectedLocation, Text);
+    }
 }
