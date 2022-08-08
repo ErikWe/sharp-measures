229dbdbdde07e3a236488dd2e95cb78646c35cad
Erik <erikwestberg7@gmail.com>
2022-08-09 00:07:41 +0200

    Moved Snapshots to Verify/Snapshots

---
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Verify/Cleanup/Cleaner.py
@@ -0,0 +1,22 @@
+﻿import argparse
+import os
+
+class Utility:
+    
+    def stateExists():
+        return os.path.isdir(os.getcwd() + '/State')
+
+class Starter:
+    
+    def run():
+        if Utility.stateExists():
+            print('Restore or finish already started cleanup before starting another process.')
+
+parser = argparse.ArgumentParser(description = 'Delete unused Verify snapshots.')
+
+parser.add_argument('mode', type = str, nargs = 1, choices = ['start', 'restore', 'finish'], help = 'The mode')
+
+args = parser.parse_args()
+
+if (args.mode[0] == 'start'):
+    Starter.run()
