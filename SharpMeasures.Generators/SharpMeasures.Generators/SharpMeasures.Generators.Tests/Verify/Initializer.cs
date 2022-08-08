782f346b1cd769ad716381b0409e205db506a4ce
Erik <erikwestberg7@gmail.com>
2022-08-09 00:07:41 +0200

    Revert "Moved Snapshots to Verify/Snapshots"
    This reverts commit 229dbdbdde07e3a236488dd2e95cb78646c35cad.

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Verify/Initializer.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Verify/Initializer.cs
@@ -57,7 +57,7 @@ internal static class Initializer
         string[] path = new string[]
         {
             projectDirectory,
-            @"Verify\Snapshots"
+            @"Snapshots"
         };
 
         path = path.Concat(type.FullName?.Split('.').Skip(3) ?? Array.Empty<string>()).ToArray();
