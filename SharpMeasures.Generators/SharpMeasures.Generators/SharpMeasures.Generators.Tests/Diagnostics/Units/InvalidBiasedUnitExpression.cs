36a75f256cfcf05c4255b0253dd162fdaa25ae11
Erik <erikwestberg7@gmail.com>
2022-08-08 20:05:01 +0200

    Passing InvalidScaled/BiasedUnitExpression tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/InvalidBiasedUnitExpression.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/InvalidBiasedUnitExpression.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Units;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,21 +15,22 @@ using Xunit;
 public class InvalidBiasedUnitExpression
 {
     [Fact]
-    public Task Null_ExactListAndVerify()
-    {
-        string source = SourceTexts.Biased(bias: "null");
-
-        return AssertExactlyBiasedUnitExpressionDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
+    public Task VerifyInvalidBiasedUnitExpressionDiagnosticsMessage_Null() => Assert(NullExpression).VerifyDiagnostics();
 
     [Fact]
-    public Task Empty_ExactListAndVerify()
-    {
-        string source = SourceTexts.Biased(bias: "\"\"");
+    public Task VerifyInvalidBiasedUnitExpressionDiagnosticsMessage_Empty() => Assert(EmptyExpression).VerifyDiagnostics();
 
-        return AssertExactlyBiasedUnitExpressionDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
+    private static SourceSubtext NullExpression { get; } = SourceSubtext.Covered("null");
+    private static SourceSubtext EmptyExpression { get; } = SourceSubtext.Covered("\"\"");
 
-    private static GeneratorVerifier AssertExactlyBiasedUnitExpressionDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(InvalidBiasedUnitExpressionDiagnostics).AssertAllDiagnosticsValidLocation();
+    private static GeneratorVerifier AssertExactlyInvalidBiasedUnitExpressionDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(InvalidBiasedUnitExpressionDiagnostics);
     private static IReadOnlyCollection<string> InvalidBiasedUnitExpressionDiagnostics { get; } = new string[] { DiagnosticIDs.InvalidBiasedUnitExpression };
+
+    private static GeneratorVerifier Assert(SourceSubtext expression)
+    {
+        var source = SourceTexts.Biased(bias: expression.ToString());
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(source, expression.Context.With(outerPrefix: "[BiasedUnit(\"Celsius\", \"Celsius\", \"Kelvin\", "));
+
+        return AssertExactlyInvalidBiasedUnitExpressionDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
 }
--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/InvalidScaledUnitExpression.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Units/InvalidScaledUnitExpression.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Units;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,21 +15,22 @@ using Xunit;
 public class InvalidScaledUnitExpression
 {
     [Fact]
-    public Task Null_ExactListAndVerify()
-    {
-        string source = SourceTexts.Scaled(value: "null");
-
-        return AssertExactlyScaledUnitExpressionDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
+    public Task VerifyInvalidScaledUnitExpressionDiagnosticsMessage_Null() => Assert(NullExpression).VerifyDiagnostics();
 
     [Fact]
-    public Task Empty_ExactListAndVerify()
-    {
-        string source = SourceTexts.Scaled(value: "\"\"");
+    public Task VerifyInvalidScaledUnitExpressionDiagnosticsMessage_Empty() => Assert(EmptyExpression).VerifyDiagnostics();
 
-        return AssertExactlyScaledUnitExpressionDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
+    private static SourceSubtext NullExpression { get; } = SourceSubtext.Covered("null");
+    private static SourceSubtext EmptyExpression { get; } = SourceSubtext.Covered("\"\"");
 
-    private static GeneratorVerifier AssertExactlyScaledUnitExpressionDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(InvalidScaledUnitExpressionDiagnostics).AssertAllDiagnosticsValidLocation();
+    private static GeneratorVerifier AssertExactlyInvalidScaledUnitExpressionDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(InvalidScaledUnitExpressionDiagnostics);
     private static IReadOnlyCollection<string> InvalidScaledUnitExpressionDiagnostics { get; } = new string[] { DiagnosticIDs.InvalidScaledUnitExpression };
+
+    private static GeneratorVerifier Assert(SourceSubtext expression)
+    {
+        var source = SourceTexts.Scaled(value: expression.ToString());
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(source, expression.Context.With(outerPrefix: "[ScaledUnit(\"Kilometre\", \"Kilometres\", \"Metre\", "));
+
+        return AssertExactlyInvalidScaledUnitExpressionDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
 }
