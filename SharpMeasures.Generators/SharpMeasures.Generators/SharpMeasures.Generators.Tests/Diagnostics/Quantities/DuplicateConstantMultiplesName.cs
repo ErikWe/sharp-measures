f788187b27f63a3173e5d57bf7d1791e9bb49907
Erik <erikwestberg7@gmail.com>
2022-08-09 23:32:00 +0200

    Passing DuplicateConstantName/Multiples and ConstantSharesNameWithUnit tests

--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantMultiplesName.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantMultiplesName.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Quantities;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,65 +15,56 @@ using Xunit;
 public class DuplicateConstantMultiplesName
 {
     [Fact]
-    public Task VerifyDuplicateConstantMultiplesNameDiagnosticsMessage()
-    {
-        string source = ScalarText(", Multiples = \"MultiplesOfPlancks\"", ", Multiples = \"MultiplesOfPlancks\"");
-
-        return AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source).VerifyDiagnostics();
-    }
+    public Task VerifyDuplicateConstantMultiplesNameDiagnosticsMessage() => AssertScalar(ScalarBothExplicit.FirstArgument, ScalarBothExplicit.SecondArgument, ScalarBothExplicit.TargetAttribute).VerifyDiagnostics();
 
     [Theory]
-    [MemberData(nameof(DuplicateConstantMultiplesNames))]
-    public void Scalar_ExactList(string firstArgument, string secondArgument)
-    {
-        string source = ScalarText(firstArgument, secondArgument);
-
-        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
-    }
+    [MemberData(nameof(DuplicateScalarConstantMultiplesNames))]
+    public void Scalar(string firstArgument, SourceSubtext secondArgument, bool targetAttribute) => AssertScalar(firstArgument, secondArgument, targetAttribute);
 
     [Theory]
-    [MemberData(nameof(DuplicateConstantMultiplesNames))]
-    public void SpecializedScalar_ExactList(string firstArgument, string secondArgument)
-    {
-        string source = SpecializedScalarText(firstArgument, secondArgument);
-
-        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
-    }
+    [MemberData(nameof(DuplicateScalarConstantMultiplesNames))]
+    public void SpecializedScalar(string firstArgument, SourceSubtext secondArgument, bool targetAttribute) => AssertSpecializedScalar(firstArgument, secondArgument, targetAttribute);
 
     [Theory]
-    [MemberData(nameof(DuplicateConstantMultiplesNames))]
-    public void Vector_ExactList(string firstArgument, string secondArgument)
-    {
-        string source = VectorText(firstArgument, secondArgument);
-
-        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
-    }
+    [MemberData(nameof(DuplicateVectorConstantMultiplesNames))]
+    public void Vector(string firstArgument, SourceSubtext secondArgument, bool targetAttribute) => AssertVector(firstArgument, secondArgument, targetAttribute);
 
     [Theory]
-    [MemberData(nameof(DuplicateConstantMultiplesNames))]
-    public void SpecializedVector_ExactList(string firstArgument, string secondArgument)
-    {
-        string source = SpecializedVectorText(firstArgument, secondArgument);
-
-        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
-    }
+    [MemberData(nameof(DuplicateVectorConstantMultiplesNames))]
+    public void SpecializedVector(string firstArgument, SourceSubtext secondArgument, bool targetAttribute) => AssertSpecializedVector(firstArgument, secondArgument, targetAttribute);
 
     [Theory]
-    [MemberData(nameof(DuplicateConstantMultiplesNames))]
-    public void VectorGroupMember_ExactList(string firstArgument, string secondArgument)
-    {
-        string source = VectorGroupMemberText(firstArgument, secondArgument);
+    [MemberData(nameof(DuplicateVectorConstantMultiplesNames))]
+    public void VectorGroupMember(string firstArgument, SourceSubtext secondArgument, bool targetAttribute) => AssertVectorGroupMember(firstArgument, secondArgument, targetAttribute);
 
-        AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(source);
-    }
-
-    private static IEnumerable<object[]> DuplicateConstantMultiplesNames() => new object[][]
+    public static IEnumerable<object[]> DuplicateScalarConstantMultiplesNames() => new object[][]
     {
-        new[] { ", Multiples = \"MultiplesOfPlancks\"", ", Multiples = \"MultiplesOfPlancks\"" },
-        new[] { string.Empty, ", Multiples = \"MultiplesOfPlancks\"" }
+        new object[] { ScalarBothExplicit.FirstArgument, ScalarBothExplicit.SecondArgument, ScalarBothExplicit.TargetAttribute },
+        new object[] { ScalarFirstExplicit.FirstArgument, ScalarFirstExplicit.SecondArgument, ScalarFirstExplicit.TargetAttribute },
+        new object[] { ScalarSecondExplicit.FirstArgument, ScalarSecondExplicit.SecondArgument, ScalarSecondExplicit.TargetAttribute }
     };
 
-    private static GeneratorVerifier AssertExactlyDuplicateConstantMultiplesNameDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantMultiplesNameDiagnostics).AssertAllDiagnosticsValidLocation();
+    public static IEnumerable<object[]> DuplicateVectorConstantMultiplesNames() => new object[][]
+    {
+        new object[] { VectorBothExplicit.FirstArgument, VectorBothExplicit.SecondArgument, VectorBothExplicit.TargetAttribute },
+        new object[] { VectorFirstExplicit.FirstArgument, VectorFirstExplicit.SecondArgument, VectorFirstExplicit.TargetAttribute },
+        new object[] { VectorSecondExplicit.FirstArgument, VectorSecondExplicit.SecondArgument, VectorSecondExplicit.TargetAttribute }
+    };
+
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) ScalarBothExplicit => (ScalarFirstAttributeMultiples.Text, ScalarFirstAttributeMultiples, false);
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) ScalarFirstExplicit => (string.Empty, ScalarFirstAttributeMultiples, false);
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) ScalarSecondExplicit => (ScalarSecondAttributeMultiples.Text, SourceSubtext.Covered(string.Empty), true);
+
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) VectorBothExplicit => (VectorFirstAttributeMultiples.Text, VectorFirstAttributeMultiples, false);
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) VectorFirstExplicit => (string.Empty, VectorFirstAttributeMultiples, false);
+    private static (string FirstArgument, SourceSubtext SecondArgument, bool TargetAttribute) VectorSecondExplicit => (VectorSecondAttributeMultiples.Text, SourceSubtext.Covered(string.Empty), true);
+
+    private static SourceSubtext ScalarFirstAttributeMultiples { get; } = SourceSubtext.Covered("\"MultiplesOfPlanck\"", prefix: ", Multiples = ");
+    private static SourceSubtext ScalarSecondAttributeMultiples { get; } = SourceSubtext.Covered("\"MultiplesOfPlanck2\"", prefix: ", Multiples = ");
+    private static SourceSubtext VectorFirstAttributeMultiples { get; } = SourceSubtext.Covered("\"MultiplesOfMetreOnes\"", prefix: ", Multiples = ");
+    private static SourceSubtext VectorSecondAttributeMultiples { get; } = SourceSubtext.Covered("\"MultiplesOfMetreOnes2\"", prefix: ", Multiples = ");
+
+    private static GeneratorVerifier AssertExactlyDuplicateConstantMultiplesNameDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantMultiplesNameDiagnostics);
     private static IReadOnlyCollection<string> DuplicateConstantMultiplesNameDiagnostics { get; } = new string[] { DiagnosticIDs.DuplicateConstantMultiplesName };
 
     private static string ScalarText(string firstArgument, string secondArgument) => $$"""
@@ -88,6 +80,17 @@ public class DuplicateConstantMultiplesName
         [SharpMeasuresUnit(typeof(Length))]
         public partial class UnitOfLength { }
         """;
+
+    private static GeneratorVerifier AssertScalar(string firstArgument, SourceSubtext secondArgument, bool targetAttribute)
+    {
+        var source = ScalarText(firstArgument, secondArgument.Text);
+
+        var expectedLocation = targetAttribute
+            ? ExpectedDiagnosticsLocation.TextSpan(source, target: "ScalarConstant", postfix: "(\"Planck2\"")
+            : ExpectedDiagnosticsLocation.TextSpan(source, secondArgument.Context.With(outerPrefix: "(\"Planck2\", \"Metre\", 1.616255E-35"));
+
+        return AssertExactlyDuplicateConstantMultiplesNameDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
 
     private static string SpecializedScalarText(string firstArgument, string secondArgument) => $$"""
         using SharpMeasures.Generators.Scalars;
@@ -106,13 +109,24 @@ public class DuplicateConstantMultiplesName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertSpecializedScalar(string firstArgument, SourceSubtext secondArgument, bool targetAttribute)
+    {
+        var source = SpecializedScalarText(firstArgument, secondArgument.Text);
+
+        var expectedLocation = targetAttribute
+            ? ExpectedDiagnosticsLocation.TextSpan(source, target: "ScalarConstant", postfix: "(\"Planck2\"")
+            : ExpectedDiagnosticsLocation.TextSpan(source, secondArgument.Context.With(outerPrefix: "(\"Planck2\", \"Metre\", 1.616255E-35"));
+
+        return AssertExactlyDuplicateConstantMultiplesNameDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
+
     private static string VectorText(string firstArgument, string secondArgument) => $$"""
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
-        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
-        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1{{firstArgument}})]
+        [VectorConstant("MetreOnes2", "Metre", 1, 1, 1{{secondArgument}})]
         [SharpMeasuresVector(typeof(UnitOfLength))]
         public partial class Position3 { }
 
@@ -124,13 +138,24 @@ public class DuplicateConstantMultiplesName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertVector(string firstArgument, SourceSubtext secondArgument, bool targetAttribute)
+    {
+        var source = VectorText(firstArgument, secondArgument.Text);
+
+        var expectedLocation = targetAttribute
+            ? ExpectedDiagnosticsLocation.TextSpan(source, target: "VectorConstant", postfix: "(\"MetreOnes2\"")
+            : ExpectedDiagnosticsLocation.TextSpan(source, secondArgument.Context.With(outerPrefix: "(\"MetreOnes2\", \"Metre\", 1, 1, 1"));
+
+        return AssertExactlyDuplicateConstantMultiplesNameDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
+
     private static string SpecializedVectorText(string firstArgument, string secondArgument) => $$"""
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
-        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
-        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1{{firstArgument}})]
+        [VectorConstant("MetreOnes2", "Metre", 1, 1, 1{{secondArgument}})]
         [SpecializedSharpMeasuresVector(typeof(Position3))]
         public partial class Displacement3 { }
 
@@ -145,13 +170,24 @@ public class DuplicateConstantMultiplesName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertSpecializedVector(string firstArgument, SourceSubtext secondArgument, bool targetAttribute)
+    {
+        var source = SpecializedVectorText(firstArgument, secondArgument.Text);
+
+        var expectedLocation = targetAttribute
+            ? ExpectedDiagnosticsLocation.TextSpan(source, target: "VectorConstant", postfix: "(\"MetreOnes2\"")
+            : ExpectedDiagnosticsLocation.TextSpan(source, secondArgument.Context.With(outerPrefix: "(\"MetreOnes2\", \"Metre\", 1, 1, 1"));
+
+        return AssertExactlyDuplicateConstantMultiplesNameDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
+
     private static string VectorGroupMemberText(string firstArgument, string secondArgument) => $$"""
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
-        [ScalarConstant("Planck", "Metre", 1.616255E-35{{firstArgument}})]
-        [ScalarConstant("Planck2", "Metre", 1.616255E-35{{secondArgument}})]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1{{firstArgument}})]
+        [VectorConstant("MetreOnes2", "Metre", 1, 1, 1{{secondArgument}})]
         [SharpMeasuresVectorGroupMember(typeof(Position))]
         public partial class Position3 { }
 
@@ -165,4 +201,15 @@ public class DuplicateConstantMultiplesName
         [SharpMeasuresUnit(typeof(Length))]
         public partial class UnitOfLength { }
         """;
+
+    private static GeneratorVerifier AssertVectorGroupMember(string firstArgument, SourceSubtext secondArgument, bool targetAttribute)
+    {
+        var source = VectorGroupMemberText(firstArgument, secondArgument.Text);
+
+        var expectedLocation = targetAttribute
+            ? ExpectedDiagnosticsLocation.TextSpan(source, target: "VectorConstant", postfix: "(\"MetreOnes2\"")
+            : ExpectedDiagnosticsLocation.TextSpan(source, secondArgument.Context.With(outerPrefix: "(\"MetreOnes2\", \"Metre\", 1, 1, 1"));
+
+        return AssertExactlyDuplicateConstantMultiplesNameDiagnostics(source).AssertDiagnosticsLocation(expectedLocation, source);
+    }
 }
--- SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantName.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators/SharpMeasures.Generators.Tests/Diagnostics/Quantities/DuplicateConstantName.cs
@@ -1,6 +1,7 @@
 ﻿namespace SharpMeasures.Generators.Tests.Diagnostics.Quantities;
 
 using SharpMeasures.Generators.Diagnostics;
+using SharpMeasures.Generators.Tests.Utility;
 using SharpMeasures.Generators.Tests.Verify;
 
 using System.Collections.Generic;
@@ -14,36 +15,21 @@ using Xunit;
 public class DuplicateConstantName
 {
     [Fact]
-    public Task Scalar_ExactListAndVerify()
-    {
-        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(ScalarText).VerifyDiagnostics();
-    }
+    public Task Scalar() => AssertScalar().VerifyDiagnostics();
 
     [Fact]
-    public Task SpecializedScalar_ExactListAndVerify()
-    {
-        return AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(SpecializedScalarText).VerifyDiagnostics();
-    }
+    public void SpecializedScalar() => AssertSpecializedScalar();
 
     [Fact]
-    public void Vector_ExactList()
-    {
-        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(VectorText);
-    }
+    public void Vector() => AssertVector();
 
     [Fact]
-    public void SpecializedVector_ExactList()
-    {
-        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(SpecializedVectorText);
-    }
+    public void SpecializedVector() => AssertSpecializedVector();
 
     [Fact]
-    public void VectorGroupMember_ExactList()
-    {
-        AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(VectorGroupMemberText);
-    }
+    public void VectorGroupMember() => AssertVectorGroupMember();
 
-    private static GeneratorVerifier AssertExactlyDuplicateConstantNameDiagnosticsWithValidLocation(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantNameDiagnostics).AssertAllDiagnosticsValidLocation();
+    private static GeneratorVerifier AssertExactlyDuplicateConstantNameDiagnostics(string source) => GeneratorVerifier.Construct<SharpMeasuresGenerator>(source).AssertExactlyListedDiagnosticsIDsReported(DuplicateConstantNameDiagnostics);
     private static IReadOnlyCollection<string> DuplicateConstantNameDiagnostics { get; } = new string[] { DiagnosticIDs.DuplicateConstantName };
 
     private static string ScalarText => """
@@ -51,7 +37,7 @@ public class DuplicateConstantName
         using SharpMeasures.Generators.Units;
 
         [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)] // <-
         [SharpMeasuresScalar(typeof(UnitOfLength))]
         public partial class Length { }
             
@@ -60,12 +46,19 @@ public class DuplicateConstantName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertScalar()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(ScalarText, target: "\"Planck\"", postfix: ", \"Metre\", 1.616255E-35)] // <-");
+
+        return AssertExactlyDuplicateConstantNameDiagnostics(ScalarText).AssertDiagnosticsLocation(expectedLocation, ScalarText);
+    }
+
     private static string SpecializedScalarText => """
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
 
         [ScalarConstant("Planck", "Metre", 1.616255E-35)]
-        [ScalarConstant("Planck", "Metre", 1.616255E-35)]
+        [ScalarConstant("Planck", "Metre", 1.616255E-35)] // <-
         [SpecializedSharpMeasuresScalar(typeof(Length))]
         public partial class Distance { }
 
@@ -77,13 +70,20 @@ public class DuplicateConstantName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertSpecializedScalar()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(SpecializedScalarText, target: "\"Planck\"", postfix: ", \"Metre\", 1.616255E-35)] // <-");
+
+        return AssertExactlyDuplicateConstantNameDiagnostics(SpecializedScalarText).AssertDiagnosticsLocation(expectedLocation, SpecializedScalarText);
+    }
+
     private static string VectorText => """
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
         [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)] // <-
         [SharpMeasuresVector(typeof(UnitOfLength))]
         public partial class Position3 { }
 
@@ -95,13 +95,20 @@ public class DuplicateConstantName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertVector()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(VectorText, target: "\"MetreOnes\"", postfix: ", \"Metre\", 1, 1, 1)] // <-");
+
+        return AssertExactlyDuplicateConstantNameDiagnostics(VectorText).AssertDiagnosticsLocation(expectedLocation, VectorText);
+    }
+
     private static string SpecializedVectorText => """
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
         [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)] // <-
         [SpecializedSharpMeasuresVector(typeof(Position3))]
         public partial class Displacement3 { }
 
@@ -116,21 +123,38 @@ public class DuplicateConstantName
         public partial class UnitOfLength { }
         """;
 
+    private static GeneratorVerifier AssertSpecializedVector()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(SpecializedVectorText, target: "\"MetreOnes\"", postfix: ", \"Metre\", 1, 1, 1)] // <-");
+
+        return AssertExactlyDuplicateConstantNameDiagnostics(SpecializedVectorText).AssertDiagnosticsLocation(expectedLocation, SpecializedVectorText);
+    }
+
     private static string VectorGroupMemberText => """
         using SharpMeasures.Generators.Scalars;
         using SharpMeasures.Generators.Units;
         using SharpMeasures.Generators.Vectors;
 
         [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
-        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)]
+        [VectorConstant("MetreOnes", "Metre", 1, 1, 1)] // <-
         [SharpMeasuresVectorGroupMember(typeof(Position))]
         public partial class Position3 { }
 
         [SharpMeasuresVectorGroup(typeof(UnitOfLength))]
         public static partial class Position { }
+
+        [SharpMeasuresScalar(typeof(UnitOfLength))]
+        public partial class Length { }
             
         [FixedUnit("Metre", "Metres")]
         [SharpMeasuresUnit(typeof(Length))]
         public partial class UnitOfLength { }
         """;
+
+    private static GeneratorVerifier AssertVectorGroupMember()
+    {
+        var expectedLocation = ExpectedDiagnosticsLocation.TextSpan(VectorGroupMemberText, target: "\"MetreOnes\"", postfix: ", \"Metre\", 1, 1, 1)] // <-");
+
+        return AssertExactlyDuplicateConstantNameDiagnostics(VectorGroupMemberText).AssertDiagnosticsLocation(expectedLocation, VectorGroupMemberText);
+    }
 }
