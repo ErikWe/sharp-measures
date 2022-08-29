3f11876ded53a70d31ff6253c030fbb742d516e4
Erik <erikwestberg7@gmail.com>
2022-08-29 02:27:40 +0200

    ConvertibleVector execution supports non-groups

--- SharpMeasures.Generators/SharpMeasures.Generators.Vectors/Pipelines/Vector/Conversions/Execution.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators.Vectors/Pipelines/Vector/Conversions/Execution.cs
@@ -16,6 +16,11 @@ internal static class Execution
     {
         string source = Composer.Compose(data);
 
+        if (source.Length is 0)
+        {
+            return;
+        }
+
         context.AddSource($"{data.Vector.Name}_Conversions.g.cs", SourceText.From(source, Encoding.UTF8));
     }
 
@@ -32,6 +37,8 @@ internal static class Execution
         private NewlineSeparationHandler SeparationHandler { get; }
 
         private DataModel Data { get; }
+
+        private bool AnyConversions { get; set; }
 
         private Composer(DataModel data)
         {
@@ -53,46 +60,61 @@ internal static class Execution
 
         private string Retrieve()
         {
-            return Builder.ToString();
+            if (AnyConversions)
+            {
+                return Builder.ToString();
+            }
+
+            return string.Empty;
         }
 
         private void ComposeTypeBlock(Indentation indentation)
         {
             SeparationHandler.MarkUnncecessary();
 
-            foreach (var convertibleVectorGroup in Data.Conversions.SelectMany(static (x) => x.Vectors))
+            foreach (var convertibleVector in Data.Conversions.SelectMany(static (x) => x.Vectors))
             {
-                if (Data.VectorPopulation.Groups.TryGetValue(convertibleVectorGroup, out var group))
+                if (Data.VectorPopulation.Groups.TryGetValue(convertibleVector, out var group))
                 {
                     if (group.MembersByDimension.TryGetValue(Data.Dimension, out var correspondingMember))
                     {
                         AppendInstanceConversions(correspondingMember, indentation);
                     }
                 }
+
+                if (Data.VectorPopulation.Vectors.ContainsKey(convertibleVector))
+                {
+                    AppendInstanceConversions(convertibleVector, indentation);
+                }
             }
 
-            foreach (var convertibleVectorGroups in Data.Conversions)
+            foreach (var convertibleVectors in Data.Conversions)
             {
-                if (convertibleVectorGroups.CastOperatorBehaviour is ConversionOperatorBehaviour.None)
+                if (convertibleVectors.CastOperatorBehaviour is ConversionOperatorBehaviour.None)
                 {
                     continue;
                 }
 
-                Action<NamedType, Indentation> composer = convertibleVectorGroups.CastOperatorBehaviour switch
+                Action<NamedType, Indentation> composer = convertibleVectors.CastOperatorBehaviour switch
                 {
                     ConversionOperatorBehaviour.Explicit => AppendExplicitOperatorConversion,
                     ConversionOperatorBehaviour.Implicit => AppendImplicitOperatorConversion,
                     _ => throw new NotSupportedException("Invalid cast operation")
                 };
 
-                foreach (var convertibleVectorGroup in convertibleVectorGroups.Vectors)
+                foreach (var convertibleVector in convertibleVectors.Vectors)
                 {
-                    if (Data.VectorPopulation.Groups.TryGetValue(convertibleVectorGroup, out var group))
+                    if (Data.VectorPopulation.Groups.TryGetValue(convertibleVector, out var group))
                     {
                         if (group.MembersByDimension.TryGetValue(Data.Dimension, out var correspondingMember))
                         {
                             composer(correspondingMember, indentation);
                         }
+                    }
+
+                    if (Data.VectorPopulation.Vectors.ContainsKey(convertibleVector))
+                    {
+                        composer(convertibleVector, indentation);
                     }
                 }
             }
@@ -100,6 +122,8 @@ internal static class Execution
 
         private void AppendInstanceConversions(NamedType vectorGroupMember, Indentation indentation)
         {
+            AnyConversions = true;
+
             SeparationHandler.AddIfNecessary();
 
             AppendDocumentation(indentation, Data.Documentation.Conversion(vectorGroupMember));
