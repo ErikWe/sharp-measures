12f139fce959cfd2da711c709144692c3a943b29
Erik <erikwestberg7@gmail.com>
2022-09-09 01:57:28 +0200

    Correctly interpret nested namespaces

--- SharpMeasures.Generators/SharpMeasures.Generators.RoslynUtility/NamedType.cs
+++ SharpMeasures.Generators/SharpMeasures.Generators.RoslynUtility/NamedType.cs
@@ -8,10 +8,27 @@ public readonly record struct NamedType(string Name, string Namespace, bool IsVa
 
     internal static NamedType FromSymbol(INamedTypeSymbol symbol)
     {
-        return new(symbol.Name, symbol.ContainingNamespace?.Name ?? string.Empty, symbol.IsValueType);
+        return new(symbol.Name, GetNamespace(symbol), symbol.IsValueType);
     }
 
     public bool IsReferenceType => IsValueType is false;
 
     public string FullyQualifiedName => $"global::{(string.IsNullOrEmpty(Namespace) ? string.Empty : $"{Namespace}.")}{Name}";
+
+    private static string GetNamespace(ISymbol symbol)
+    {
+        if (symbol.ContainingNamespace is null || symbol.ContainingNamespace.Name.Length is 0)
+        {
+            return string.Empty;
+        }
+
+        string containingNamespace = GetNamespace(symbol.ContainingNamespace);
+
+        if (containingNamespace.Length is not 0)
+        {
+            return $"{containingNamespace}.{symbol.ContainingNamespace.Name}";
+        }
+
+        return symbol.ContainingNamespace.Name;
+    }
 }
