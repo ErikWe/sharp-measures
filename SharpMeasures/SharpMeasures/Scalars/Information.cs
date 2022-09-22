c17deaae0d5fa52f9c99454dde95cceb110af8b7
Erik <erikwestberg7@gmail.com>
2022-09-22 05:04:30 +0200

    Added Information quantity w/ unit

---
+++ SharpMeasures/SharpMeasures/Scalars/Information.cs
@@ -0,0 +1,6 @@
+﻿namespace SharpMeasures;
+
+using SharpMeasures.Generators.Scalars;
+
+[SharpMeasuresScalar(typeof(UnitOfInformation), DefaultUnitInstanceName = "Bit", DefaultUnitInstanceSymbol = "b")]
+public readonly partial record struct Information { }
---
+++ SharpMeasures/SharpMeasures/Units/UnitOfInformation.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpMeasures;
+
+using SharpMeasures.Generators.Units;
+
+[FixedUnitInstance("Bit", "[*]s")]
+[PrefixedUnitInstance("Kilobit", "[*]s", "Bit", MetricPrefixName.Kilo)]
+[PrefixedUnitInstance("Megabit", "[*]s", "Bit", MetricPrefixName.Mega)]
+[PrefixedUnitInstance("Gigabit", "[*]s", "Bit", MetricPrefixName.Giga)]
+[PrefixedUnitInstance("Terabit", "[*]s", "Bit", MetricPrefixName.Tera)]
+[PrefixedUnitInstance("Kibibit", "[*]s", "Bit", BinaryPrefixName.Kibi)]
+[PrefixedUnitInstance("Mebibit", "[*]s", "Bit", BinaryPrefixName.Mebi)]
+[PrefixedUnitInstance("Gibibit", "[*]s", "Bit", BinaryPrefixName.Gibi)]
+[PrefixedUnitInstance("Tebibit", "[*]s", "Bit", BinaryPrefixName.Tebi)]
+[ScaledUnitInstance("Byte", "[*]s", "Bit", 8)]
+[PrefixedUnitInstance("Kilobyte", "[*]s", "Byte", MetricPrefixName.Kilo)]
+[PrefixedUnitInstance("Megabyte", "[*]s", "Byte", MetricPrefixName.Mega)]
+[PrefixedUnitInstance("Gigabyte", "[*]s", "Byte", MetricPrefixName.Giga)]
+[PrefixedUnitInstance("Terabyte", "[*]s", "Byte", MetricPrefixName.Tera)]
+[PrefixedUnitInstance("Kibibyte", "[*]s", "Byte", BinaryPrefixName.Kibi)]
+[PrefixedUnitInstance("Mebibyte", "[*]s", "Byte", BinaryPrefixName.Mebi)]
+[PrefixedUnitInstance("Gibibyte", "[*]s", "Byte", BinaryPrefixName.Gibi)]
+[PrefixedUnitInstance("Tebibyte", "[*]s", "Byte", BinaryPrefixName.Tebi)]
+[SharpMeasuresUnit(typeof(Information))]
+public readonly partial record struct UnitOfInformation { }
