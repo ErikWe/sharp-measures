25fef38b8853603deeef189922fc5b8fccc08411
Erik <erikwestberg7@gmail.com>
2022-10-04 17:10:57 +0200

    EquatableX collections check if type is already EquatableX

--- SharpMeasures.Generators/SharpMeasures.Equatables/Extensions/Dictionary.cs
+++ SharpMeasures.Generators/SharpMeasures.Equatables/Extensions/Dictionary.cs
@@ -4,6 +4,23 @@ using System.Collections.Generic;
 
 public static partial class EquatableExtensions
 {
-    public static EquatableDictionary<TKey, TValue> AsEquatable<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) => new(dictionary);
-    public static ReadOnlyEquatableDictionary<TKey, TValue> AsReadOnlyEquatable<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> set) => new(set);
+    public static EquatableDictionary<TKey, TValue> AsEquatable<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+    {
+        if (dictionary is EquatableDictionary<TKey, TValue> equatableDictionary)
+        {
+            return equatableDictionary;
+        }
+
+        return new(dictionary);
+    }
+
+    public static ReadOnlyEquatableDictionary<TKey, TValue> AsReadOnlyEquatable<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary)
+    {
+        if (dictionary is ReadOnlyEquatableDictionary<TKey, TValue> equatableDictionary)
+        {
+            return equatableDictionary;
+        }
+
+        return new(dictionary);
+    }
 }
--- SharpMeasures.Generators/SharpMeasures.Equatables/Extensions/IList.cs
+++ SharpMeasures.Generators/SharpMeasures.Equatables/Extensions/IList.cs
@@ -4,6 +4,23 @@ using System.Collections.Generic;
 
 public static partial class EquatableExtensions
 {
-    public static EquatableList<T> AsEquatable<T>(this IList<T> list) => new(list);
-    public static ReadOnlyEquatableList<T> AsReadOnlyEquatable<T>(this IReadOnlyList<T> list) => new(list);
+    public static EquatableList<T> AsEquatable<T>(this IList<T> list)
+    {
+        if (list is EquatableList<T> equatableList)
+        {
+            return equatableList;
+        }
+
+        return new(list);
+    }
+
+    public static ReadOnlyEquatableList<T> AsReadOnlyEquatable<T>(this IReadOnlyList<T> list)
+    {
+        if (list is ReadOnlyEquatableList<T> equatableList)
+        {
+            return equatableList;
+        }
+
+        return new(list);
+    }
 }
