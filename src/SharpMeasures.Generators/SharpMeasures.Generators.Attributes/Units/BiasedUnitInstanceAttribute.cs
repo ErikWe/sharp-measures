10955883e682bf5815d91539373fde3e1e8005ef
ErikWe <erikwestberg7@gmail.com>
2023-04-10 00:29:40 +0200

    Removed complex plural-form options from unit instance attributes + docs improvements

--- src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Units/BiasedUnitInstanceAttribute.cs
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Units/BiasedUnitInstanceAttribute.cs
@@ -1,35 +1,35 @@
-﻿namespace SharpMeasures.Generators;
+﻿namespace SharpMeasures;
 
 using System;
 
-/// <summary>Defines an instance of a unit according to a bias relative to another instance of the same unit.</summary>
+/// <summary>Applied to SharpMeasures units, defining an instance of the unit using a bias relative to another instance of the same unit.</summary>
+/// <remarks>Defining a biased unit instance requires the unit to include a bias term.</remarks>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
 public sealed class BiasedUnitInstanceAttribute : Attribute
 {
     /// <inheritdoc cref="FixedUnitInstanceAttribute.Name"/>
     public string Name { get; }
-    /// <summary><inheritdoc cref="FixedUnitInstanceAttribute.PluralForm" path="/summary"/></summary>
-    /// <remarks>If <see cref="PluralFormRegexSubstitution"/> is set, this value is used as a .NET regex pattern. Alternatively, see <see cref="CommonPluralNotation"/> for some common notations for producing the plural form based on the singular form.</remarks>
-    public string PluralForm { get; }
-    /// <summary>The name of the original instance, relative to which this instance is biased.</summary>
-    public string OriginalUnitInstance { get; }
-    /// <summary>The bias relative to the original instance. This represents the value of this instance when the original instance is { 0 }.</summary>
-    /// <remarks>For example, if <i>Kelvin</i> is the original instance, <i>Celsius</i> should have a bias of { -273.15 }.</remarks>
-    public double Bias { get; }
-    /// <summary>An expression that computes the bias relative to the original instance. The resulting value should represent the value of this instance when the original instance is { 0 }.</summary>
-    /// <remarks><inheritdoc cref="Bias" path="/remarks"/></remarks>
-    public string? Expression { get; }
 
-    /// <summary>Used as the .NET Regex substitution string when producing the plural form of the unit, with <see cref="PluralForm"/> being used as the .NET regex pattern.</summary>
-    /// <remarks>If this property is ignored, <see cref="PluralForm"/> will not be used as a .NET regex pattern.</remarks>
-    public string PluralFormRegexSubstitution { get; init; } = string.Empty;
+    /// <inheritdoc cref="FixedUnitInstanceAttribute.PluralForm"/>
+    public string? PluralForm { get; }
+
+    /// <summary>The name of the original unit instance, relative to which this instance is biased.</summary>
+    public string OriginalUnitInstance { get; }
+
+    /// <summary>The bias relative to the original unit instance, representing the value of this unit instance when the original unit instance is { 0 }.</summary>
+    /// <remarks>For example, if <i>Kelvin</i> is the original unit instance, <i>Celsius</i> should have a bias of { -273.15 }.</remarks>
+    public double Bias { get; }
+
+    /// <summary>An expression that computes the bias relative to the original unit instance. The resulting value should represent the value of this unit instance when the original unit instance is { 0 }.</summary>
+    /// <remarks><inheritdoc cref="Bias" path="/remarks"/></remarks>
+    public string? BiasExpression { get; }
 
     /// <inheritdoc cref="BiasedUnitInstanceAttribute"/>
-    /// <param name="name"><inheritdoc cref="Name" path="/summary"/><para><inheritdoc cref="Name" path="/remarks"/></para></param>
-    /// <param name="pluralForm"><inheritdoc cref="PluralForm" path="/summary"/><para><inheritdoc cref="PluralForm" path="/remarks"/></para></param>
-    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/><para><inheritdoc cref="OriginalUnitInstance" path="/remarks"/></para></param>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    /// <param name="pluralForm"><inheritdoc cref="PluralForm" path="/summary"/></param>
+    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/></param>
     /// <param name="bias"><inheritdoc cref="Bias" path="/summary"/><para><inheritdoc cref="Bias" path="/remarks"/></para></param>
-    public BiasedUnitInstanceAttribute(string name, string pluralForm, string originalUnitInstance, double bias)
+    public BiasedUnitInstanceAttribute(string name, string? pluralForm, string originalUnitInstance, double bias)
     {
         Name = name;
         PluralForm = pluralForm;
@@ -37,18 +37,42 @@ public sealed class BiasedUnitInstanceAttribute : Attribute
         Bias = bias;
     }
 
+    /// <inheritdoc cref="BiasedUnitInstanceAttribute"/>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/></param>
+    /// <param name="bias"><inheritdoc cref="Bias" path="/summary"/><para><inheritdoc cref="Bias" path="/remarks"/></para></param>
+    public BiasedUnitInstanceAttribute(string name, string originalUnitInstance, double bias)
+    {
+        Name = name;
+        OriginalUnitInstance = originalUnitInstance;
+        Bias = bias;
+    }
+
     /// <summary><inheritdoc cref="BiasedUnitInstanceAttribute" path="/summary"/></summary>
-    /// <param name="name"><inheritdoc cref="Name" path="/summary"/><para><inheritdoc cref="Name" path="/remarks"/></para></param>
-    /// <param name="pluralForm"><inheritdoc cref="PluralForm" path="/summary"/><para><inheritdoc cref="PluralForm" path="/remarks"/></para></param>
-    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/><para><inheritdoc cref="OriginalUnitInstance" path="/remarks"/></para></param>
-    /// <param name="expression"><inheritdoc cref="Expression" path="/summary"/><para><inheritdoc cref="Expression" path="/remarks"/></para></param>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    /// <param name="pluralForm"><inheritdoc cref="PluralForm" path="/summary"/></param>
+    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/></param>
+    /// <param name="expression"><inheritdoc cref="BiasExpression" path="/summary"/><para><inheritdoc cref="BiasExpression" path="/remarks"/></para></param>
     /// <remarks><inheritdoc cref="BiasedUnitInstanceAttribute" path="/remarks"/>
-    /// <para>When applicable, <see cref="BiasedUnitInstanceAttribute(string, string, string, double)"/> should be preferred.</para></remarks>
-    public BiasedUnitInstanceAttribute(string name, string pluralForm, string originalUnitInstance, string expression)
+    /// <para>When applicable, prefer <see cref="BiasedUnitInstanceAttribute(string, string, string, double)"/>.</para></remarks>
+    public BiasedUnitInstanceAttribute(string name, string? pluralForm, string originalUnitInstance, string expression)
     {
         Name = name;
         PluralForm = pluralForm;
         OriginalUnitInstance = originalUnitInstance;
-        Expression = expression;
+        BiasExpression = expression;
+    }
+
+    /// <summary><inheritdoc cref="BiasedUnitInstanceAttribute" path="/summary"/></summary>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    /// <param name="originalUnitInstance"><inheritdoc cref="OriginalUnitInstance" path="/summary"/></param>
+    /// <param name="expression"><inheritdoc cref="BiasExpression" path="/summary"/><para><inheritdoc cref="BiasExpression" path="/remarks"/></para></param>
+    /// <remarks><inheritdoc cref="BiasedUnitInstanceAttribute" path="/remarks"/>
+    /// <para>When applicable, prefer <see cref="BiasedUnitInstanceAttribute(string, string, double)"/>.</para></remarks>
+    public BiasedUnitInstanceAttribute(string name, string originalUnitInstance, string expression)
+    {
+        Name = name;
+        OriginalUnitInstance = originalUnitInstance;
+        BiasExpression = expression;
     }
 }
