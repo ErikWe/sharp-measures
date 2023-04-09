8a404ac365661e87ce11d71185cc93e8f3419dc0
ErikWe <erikwestberg7@gmail.com>
2023-04-10 00:34:57 +0200

    InclusionStackingMode -> FilterStackingMode, and also apply for exclusion filters

---
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/FilterStackingMode.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpMeasures;
+
+/// <summary>Determines how filters behave when stacked.</summary>
+public enum FilterStackingMode
+{
+    /// <summary>The <see cref="FilterStackingMode"/> is unknown.</summary>
+    Unknown,
+    /// <summary>Applies the filter to the result of the previous filter.</summary>
+    Keep,
+    /// <summary>Applies the filter to the restored, original, state - discarding all previous filters.</summary>
+    Restore
+}
--- src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Scalars/ExcludeUnitBasesAttribute.cs
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Scalars/ExcludeUnitBasesAttribute.cs
@@ -1,17 +1,20 @@
-﻿namespace SharpMeasures.Generators;
+﻿namespace SharpMeasures;
 
 using System;
 
-/// <summary>Applied to scalar quantities, dictating the set of units for which a static property representing the value { 1 } is <i>not</i> generated.</summary>
-/// <remarks>If this attribute is absent, all recognized units are included - unless <see cref="IncludeUnitBasesAttribute"/> is used.</remarks>
+/// <summary>Applied to SharpMeasures scalar quantities, dictating the set of units for which a static property representing the magnitude { 1 } is not implemented.</summary>
+/// <remarks>If this attribute is absent, all units are included - unless <see cref="IncludeUnitBasesAttribute"/> is used.</remarks>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
 public sealed class ExcludeUnitBasesAttribute : Attribute
 {
-    /// <summary>Names of the units for which a static property representing the value { 1 } is <i>not</i> generated.</summary>
+    /// <summary>The names of the units for which a static property representing the magnitude { 1 } is not implemented.</summary>
     public string[] ExcludedUnitBases { get; }
 
+    /// <summary>If the quantity is a specialized form of another quantity, dictates how to treat unit filters present in the other quantity. The default behaviour is <see cref="FilterStackingMode.Restore"/>.</summary>
+    public FilterStackingMode StackingMode { get; init; }
+
     /// <inheritdoc cref="ExcludeUnitBasesAttribute"/>
-    /// <param name="excludedUnitBases"><inheritdoc cref="ExcludedUnitBases" path="/summary"/><para><inheritdoc cref="ExcludedUnitBases" path="/remarks"/></para></param>
+    /// <param name="excludedUnitBases"><inheritdoc cref="ExcludedUnitBases" path="/summary"/></param>
     public ExcludeUnitBasesAttribute(params string[] excludedUnitBases)
     {
         ExcludedUnitBases = excludedUnitBases;
