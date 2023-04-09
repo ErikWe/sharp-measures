e4a3a925d869a4c4a3d8ef27f1a2d8685e457919
ErikWe <erikwestberg7@gmail.com>
2023-04-10 00:33:15 +0200

    Added UnitlessQuantityAttribute + specialized form

---
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Scalars/SpecializedUnitlessQuantityAttribute.cs
@@ -0,0 +1,68 @@
+﻿namespace SharpMeasures;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>Marks the type as an auto-generated SharpMeasures unit-less quantity, behaving as a specialized form of another quantity, <typeparamref name="TOriginal"/>.</summary>
+/// <typeparam name="TOriginal">The original quantity, of which this quantity is a specialized form.</typeparam>
+/// <remarks>The following attributes may be used to modify the quantity:
+/// <list type="bullet">
+/// <item>
+/// <term><see cref="QuantityOperationAttribute{TResult, TOther}"/></term>
+/// <description>Describes the operations { + , - , ⋅ , ÷ } supported by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityProcessAttribute{TResult}"/></term>
+/// <description>Describes a custom process implemented by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityPropertyAttribute{TResult}"/></term>
+/// <description>Describes a custom readonly property implemented by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantitySumAttribute{TSum}"/></term>
+/// <description>Describes the result of addition of two instances of the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityDifferenceAttribute{TDifference}"/></term>
+/// <description>Describes the result of subtraction of two instances of the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="ConvertibleQuantityAttribute"/></term>
+/// <description>Lists other quantities that the quantity supports conversion to and/or from.</description>
+/// </item>
+/// </list></remarks>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+[SuppressMessage("Major Code Smell", "S2326: Unused type parameters should be removed", Justification = "Used when interpreting the attribute.")]
+public sealed class SpecializedUnitlessQuantityAttribute<TOriginal> : Attribute
+{
+    /// <summary>Dictates whether the quantity allows negative magnitudes. If <see langword="false"/>, the absolute value of the provided magnitude is used. By default, the behaviour is inherited from the original quantity.</summary>
+    public bool AllowNegative { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.InheritOperations"/>
+    public bool InheritOperations { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.InheritProcesses"/>
+    public bool InheritProcesses { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.InheritProperties"/>
+    public bool InheritProperties { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.InheritConversions"/>
+    public bool InheritConversions { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.ForwardsCastOperatorBehaviour"/>
+    public ConversionOperatorBehaviour ForwardsCastOperatorBehaviour { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.BackwardsCastOperatorBehaviour"/>
+    public ConversionOperatorBehaviour BackwardsCastOperatorBehaviour { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.ImplementSum"/>
+    public bool ImplementSum { get; init; }
+
+    /// <inheritdoc cref="SpecializedScalarQuantityAttribute{TOriginal}.ImplementDifference"/>
+    public bool ImplementDifference { get; init; }
+
+    /// <inheritdoc cref="SpecializedUnitlessQuantityAttribute{TQuantity}"/>
+    public SpecializedUnitlessQuantityAttribute() { }
+}
---
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Scalars/UnitlessQuantityAttribute.cs
@@ -0,0 +1,49 @@
+﻿namespace SharpMeasures;
+
+using System;
+
+/// <summary>Marks the type as an auto-generated SharpMeasures unit-less quantity.</summary>
+/// <remarks>The following attributes may be used to modify the quantity:
+/// <list type="bullet">
+/// <item>
+/// <term><see cref="QuantityOperationAttribute{TResult, TOther}"/></term>
+/// <description>Describes the operations { + , - , ⋅ , ÷ } supported by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityProcessAttribute{TResult}"/></term>
+/// <description>Describes a custom process implemented by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityPropertyAttribute{TResult}"/></term>
+/// <description>Describes a custom readonly property implemented by the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantitySumAttribute{TSum}"/></term>
+/// <description>Describes the result of addition of two instances of the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="QuantityDifferenceAttribute{TDifference}"/></term>
+/// <description>Describes the result of subtraction of two instances of the quantity.</description>
+/// </item>
+/// <item>
+/// <term><see cref="ConvertibleQuantityAttribute"/></term>
+/// <description>Lists other quantities that the quantity supports conversion to and/or from.</description>
+/// </item>
+/// </list></remarks>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public sealed class UnitlessQuantityAttribute : Attribute
+{
+    /// <summary>Dictates whether the quantity allows negative magnitudes. If <see langword="false"/>, the absolute value of the provided magnitude is used. The default behaviour is <see langword="true"/>.</summary>
+    public bool AllowNegative { get; init; }
+
+    /// <summary>Dictates whether the quantity should support addition of two instances. The default behaviour is <see langword="true"/>.</summary>
+    /// <remarks>To specify what quantity represents the sum, use <see cref="QuantitySumAttribute{T}"/>.</remarks>
+    public bool ImplementSum { get; init; }
+
+    /// <summary>Dictates whether the quantity should support subtraction of two instances. The default behaviour is <see langword="true"/>.</summary>
+    /// <remarks>To specify what quantity represents the difference, use <see cref="QuantityDifferenceAttribute{T}"/>.</remarks>
+    public bool ImplementDifference { get; init; }
+
+    /// <inheritdoc cref="UnitlessQuantityAttribute"/>
+    public UnitlessQuantityAttribute() { }
+}
