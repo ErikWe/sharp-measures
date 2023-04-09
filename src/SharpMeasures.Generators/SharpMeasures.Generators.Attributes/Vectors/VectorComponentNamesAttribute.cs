d9e0b1dd962a658979a8acedfbbbdba0ad6c7d39
ErikWe <erikwestberg7@gmail.com>
2023-04-10 00:36:41 +0200

    Added ScalarAssociation and ComponentNames attributes for vectors

---
+++ src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes/Vectors/VectorComponentNamesAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpMeasures.Vectors;
+
+using System;
+
+/// <summary>Applied to SharpMeasures vector quantities, allowing the names of the Cartesian components to be customized.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public sealed class VectorComponentNamesAttribute : Attribute
+{
+    /// <summary>The names of the Cartesian components</summary>
+    public string[] Names { get; }
+
+    /// <inheritdoc cref="VectorComponentNamesAttribute"/>
+    /// <param name="names"><inheritdoc cref="Names" path="/summary"/></param>
+    public VectorComponentNamesAttribute(params string[] names)
+    {
+        Names = names;
+    }
+}
