252e041e3f30abddc6cd6cf27d3bf33e3d9e60c0
ErikWe <erikwestberg7@gmail.com>
2023-04-14 00:19:12 +0200

    Removed .Attributes.Parsing - solved by SharpAttributeParser package

--- src/SharpMeasures.Generators/SharpMeasures.Generators.Attributes.Parsing/IAttributeLocations.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SharpMeasures.Generators.Attributes.Parsing;
-
-public interface IOpenAttributeLocations<out TLocations> : IAttributeLocations where TLocations : IOpenAttributeLocations<TLocations>
-{
-    public abstract TLocations WithAttribute(MinimalLocation attribute, MinimalLocation attributeName);
-}
